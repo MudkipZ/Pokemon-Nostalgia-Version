cc90b18322baf4c68425829a9257ec7335f29019
Matthew Crawford <matthew.crawford@nhrec.us>
2015-03-12 16:22:30 -0400

    Pokedex Now Has A Bare-Bones Constructor

--- PNV/src/crawford/matthew/pokegen/Pokedex.java
+++ PNV/src/crawford/matthew/pokegen/Pokedex.java
@@ -6,4 +6,11 @@ public class Pokedex
 	private LinkedList<PokemonSpecies> CapturedSpecies;
 	private LinkedList<PokemonSpecies> UnCapturedSeenSpecies;
 	private LinkedList<PokemonSpecies> UnSeenSpecies;
+	
+	public Pokedex()
+	{
+		CapturedSpecies=new LinkedList<PokemonSpecies>();
+		UnCapturedSeenSpecies=new LinkedList<PokemonSpecies>();
+		UnSeenSpecies=new LinkedList<PokemonSpecies>();
+	}
 }
