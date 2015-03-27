b654fd8aa9d21c9601fc5f1b8bee2086c2044fa3
Justin <justintime@verizon.net>
2015-03-27 10:05:06 -0400

    Pulled stuff from master

---
+++ PNV/src/crawford/matthew/pokegen/Ability.java
@@ -0,0 +1,12 @@
+package crawford.matthew.pokegen;
+
+public class Ability
+{
+	private String Name;
+	
+	public Ability(String name)
+	{
+		this.Name=name;
+	}
+	
+}
---
+++ PNV/src/crawford/matthew/pokegen/AddedEffect.java
@@ -0,0 +1,13 @@
+package crawford.matthew.pokegen;
+
+public class AddedEffect
+{
+	private String Name;
+	private double Magnitude;
+	
+	public AddedEffect(String name,double magnitude)
+	{
+		this.Name=name;
+		this.Magnitude=magnitude;
+	}
+}
